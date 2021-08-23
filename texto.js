39bc5f4b6a57daaa4592b5ce4f1b8cd49c10d546
DantonMariano <65569798+DantonMariano@users.noreply.github.com>
2021-08-23 17:13:14 -0300

    Corrige Texto

--- texto.js
+++ texto.js
@@ -32,7 +32,7 @@ let startWriting = () => {
     twt.typeString(`Olá, meu nome é Danton Mariano, 
     tenho 20 anos e sou Desenvolvedor Fullstack na <a target="_blank" href="https://dataclick.com.br/site/">Dataclick</a>,
     atualmente responsável pela <a target="_blank" href="https://lojadevantagens.com.br/">Loja de Vantagens</a> e pela em breve <a target="_blank" href="https://clickersports.com.br/">Clicker Shop</a>.<br>
-    Já fiz estágio no <a target="_blank" href="https://7bbm.cbm.sc.gov.br/">Sétimo Batalhão do Quartel de Bombeiros Militar de Santa Catarina</a> como Desenvolvedor PHP
+    Já fiz estágio no <a target="_blank" href="https://7bbm.cbm.sc.gov.br/">Sétimo Batalhão de Bombeiros Militar de Santa Catarina</a> como Desenvolvedor PHP
     e já trabalhei em uma filial da <a target="_blank" href="https://www.sysmo.com.br/">Sysmo</a> como Analista de Business Intelligence utilizando QlikView.
     Essas sendo apenas algumas das minhas experiências.
     Fluente em inglês, aprendi sozinho a falar e ler, como também fui aprovado com <a target="_blank" href="https://www.cambridgeenglish.org/br/exams-and-tests/advanced/">Certificado de Cambridge</a> avançado, tirando 196 de 210 na prova.<br>
