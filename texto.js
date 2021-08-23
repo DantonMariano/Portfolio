0aed419246a07378f16b993eb95fcb62f0c427c0
DantonMariano <65569798+DantonMariano@users.noreply.github.com>
2021-08-23 17:56:26 -0300

    Corrige Cambridge

--- texto.js
+++ texto.js
@@ -34,7 +34,7 @@ let startWriting = () => {
     atualmente responsável pela <a target="_blank" href="https://lojadevantagens.com.br/">Loja de Vantagens</a> e pela em breve <a target="_blank" href="https://clickersports.com.br/">Clicker Shop</a>.<br>
     Já fiz estágio no <a target="_blank" href="https://7bbm.cbm.sc.gov.br/">Sétimo Batalhão de Bombeiros Militar de Santa Catarina</a> como Desenvolvedor PHP
     e já trabalhei em uma filial da <a target="_blank" href="https://www.sysmo.com.br/">Sysmo</a> como Analista de Business Intelligence utilizando QlikView.
-    Essas sendo apenas algumas das minhas experiências.
-    Fluente em inglês, aprendi sozinho a falar e ler, como também fui aprovado com <a target="_blank" href="https://www.cambridgeenglish.org/br/exams-and-tests/advanced/">Certificado de Cambridge</a> avançado, tirando 196 de 210 na prova.<br>
+    Essas sendo apenas algumas das minhas experiências
+    Fluente em inglês, aprendi sozinho a falar e ler, como também fui aprovado com <a target="_blank" href="https://www.cambridgeenglish.org/br/exams-and-tests/advanced/">Certificado de Cambridge</a> avançado, tirando 193 de 210 na prova.<br>
     além de ser fluente nas seguintes técnologias:`).start().callFunction(showIcones);
 }
