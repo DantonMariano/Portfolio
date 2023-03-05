21f9931eaeefcd94b0035228c568ca27f43fb3ad
dantonmariano <dantonrmariano@gmail.com>
2023-03-05 18:04:20 -0300

    Rework Portfolio

--- jq.js
+++ jq.js
@@ -1,18 +1,3 @@
-$('.carousel').slick({
-  arrows:false,
-  dots:false,
-  autoplay: true,
-  autoplaySpeed: 6000,
-  centerMode:true,
-  slidesToShow: 1,
-  infinite:false,
-});
-
 $(document).ready(()=>{
-    setTimeout(()=>{
-        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-        startWriting()
-    },
-        4747
-    )
+
 })
